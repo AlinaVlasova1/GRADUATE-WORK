163f56b1ec6ece962b61d148bfd9be71462907dd
AlinaVlasova1 <alina_vlasova_01@bk.ru>
2024-06-02 15:42:27 +0300

    add asaid, header from main

--- src/app/pages/main/asaid/asaid.component.ts
+++ src/app/pages/main/asaid/asaid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IItem} from "../../../models/item";
 
 @Component({
   selector: 'app-asaid',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asaid.component.scss']
 })
 export class AsaidComponent implements OnInit {
-
+  items: IItem[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Личный кабинет',
+        route: '',
+      },
+      {
+        label: 'Облигации',
+        route: ''
+      },
+      {
+        label: 'Акции',
+        route: ''
+      },
+      {
+        label: 'Валюта',
+        route: ''
+      },
+      {
+        label: 'Металлы',
+        route: ''
+      },
+      {
+        label: 'Настройка',
+        route: ''
+      }
+    ];
   }
 
 }
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -5,17 +5,25 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import {InputTextModule} from "primeng/inputtext";
 import { AsaidComponent } from './asaid/asaid.component';
+import {MenuModule} from "primeng/menu";
+import {TableModule} from "primeng/table";
+import {StyleClassModule} from "primeng/styleclass";
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    AsaidComponent
+    AsaidComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     MainRoutingModule,
-    InputTextModule
+    InputTextModule,
+    MenuModule,
+    TableModule,
+    StyleClassModule
   ]
 })
 export class MainModule { }
